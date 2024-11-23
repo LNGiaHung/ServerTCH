ab06cb44c1484993d372f5c0c574d356c89237a9
LNGiaHung <21520890@gm.uit.edu.vn>
2024-11-23 14:01:04 +0700

    fix: change host

--- config/swagger.js
+++ config/swagger.js
@@ -11,7 +11,7 @@ const swaggerOptions = {
         },
         servers: [
             {
-                url: "http://localhost:5000/api/v1", // Change this to your server URL
+                url: "http://44.196.160.2:5000/api/v1", // Change this to your server URL
             },
         ],
         components: {
