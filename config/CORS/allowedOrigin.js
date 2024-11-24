9bcd28f0a3156bb1d853a65c8468e70ba9dadca6
LNGiaHung <21520890@gm.uit.edu.vn>
2024-11-24 20:39:49 +0700

    update: change cors to allow all subdomain

--- config/CORS/allowedOrigin.js
+++
@@ -1,7 +0,0 @@
-const allowedOrigins = [
-  'https://tchmovie.edwardxd.site',
-  'http://localhost:5173',
-  'http://localhost:5000'
-];
-
-export default allowedOrigins;
--- middleware/credentials.js
+++
@@ -1,7 +0,0 @@
-import allowedOrigins from "../config/CORS/allowedOrigin.js";
-
-const credentials = (req, res, next) => {
-    next();
-};
-
-export default credentials;
