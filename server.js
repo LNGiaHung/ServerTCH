19c201b34ce19b5c6a4cc257ff5d4335f8979833
LNGiaHung <21520890@gm.uit.edu.vn>
2024-11-26 23:05:37 +0700

    update: update to localhost:5173

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 
 // CORS configuration
 app.use(cors({
-    origin: ['https://tchmovie.edwardxd.site', 'http://localhost:3000'],
+    origin: ['https://tchmovie.edwardxd.site', 'http://localhost:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
